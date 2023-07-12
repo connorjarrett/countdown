a027d69f56186105207aa66d84d36f103b30a022
Connor Jarrett <connor.jarrett07@gmail.com>
2023-07-12 16:53:12 +0100

    Added fullscreen

--- interact.js
+++ interact.js
@@ -27,7 +27,31 @@ export function fullscreen() {
     const button = this
     const container = this.parentNode.parentNode
 
-    
+    if (!button.dataset.on) {
+        button.dataset.on = "true"
+
+        container.style.borderRadius = 0
+        container.style.position = "fixed"
+        container.style.left = "50%"
+        container.style.top = "50%"
+        container.style.translate = "-50% -50%"
+        container.style.width = "100vw"
+        container.style.height = "100vh"
+        container.style.padding = "100px"
+        container.style.zIndex = 100
+    } else {
+        button.dataset.on = ""
+
+        container.style.borderRadius = ""
+        container.style.position = ""
+        container.style.left = ""
+        container.style.top = ""
+        container.style.translate = ""
+        container.style.width = ""
+        container.style.height = ""
+        container.style.padding = ""
+        container.style.zIndex = ""
+    }
 }
 
 export function removeCountdown() {
